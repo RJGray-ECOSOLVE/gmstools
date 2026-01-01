749c45f3bd9e6232bb03695ee8a062506c074371
Russell Gray <russell.gray@globalinitiative.net>
2026-01-01 15:51:09 +0100

    Update run_intel_dashboard.R

--- R/run_intel_dashboard.R
+++ R/run_intel_dashboard.R
@@ -9,6 +9,60 @@
 #' @param port Optional port to use for the Shiny app. If NULL, Shiny picks one.
 #' @export
 run_intel_dashboard <- function(launch.browser = TRUE, port = NULL) {
+
+  required_packages <- c(
+  "flexdashboard",
+  "shiny",
+  "shinyjs",
+  "tidyverse",
+  "leaflet",
+  "plotly",
+  "visNetwork",
+  "DT",
+  "stringr",
+  "tidyr",
+  "sf",
+  "rnaturalearth",
+  "rnaturalearthdata",
+  "htmltools",
+  "scales",
+  "gmstools",
+  "geodata",
+  "tidygeocoder",
+  "rgeoboundaries",
+  "readr",
+  "lubridate",
+  "zip",
+  "echarts4r" 
+)
+
+install_if_missing <- function(pkgs) {
+  ip <- rownames(installed.packages())
+  missing_pkgs <- pkgs[!pkgs %in% ip]
+  if (length(missing_pkgs) > 0) {
+    message("Installing missing packages: ", paste(missing_pkgs, collapse = ", "))
+    install.packages(missing_pkgs, dependencies = TRUE)
+  }
+}
+
+# If you ever want automatic updates as well, uncomment this and the call below.
+update_if_desired <- function(pkgs) {
+  op <- old.packages()
+  if (is.null(op)) return(invisible())
+  old_names <- rownames(op)
+  to_update <- pkgs[pkgs %in% old_names]
+  if (length(to_update) > 0) {
+    message("Updating packages: ", paste(to_update, collapse = ", "))
+    update.packages(ask = FALSE, oldPkgs = to_update)
+  }
+}
+
+# Install any missing packages
+install_if_missing(required_packages)
+
+# Optionally update them (commented out by default)
+# update_if_desired(required_packages)
+
   # Locate the Rmd file inside the installed package
   app_file <- system.file(
     "intel_dashboard",
@@ -38,4 +92,5 @@ run_intel_dashboard <- function(launch.browser = TRUE, port = NULL) {
     shiny_args = shiny_args
   )
 }
+
 
